82701a4f290bd4f7765d9c9a027502a6cd2f6ebb
wugengsong <374722806@qq.com>
2017-06-07 17:47:06 +0800

    first commit

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+
+// module.exports = {
+  // "plugins": {
+    // to edit target browsers: use "browserlist" field in package.json
+    // "autoprefixer": {}
+//     require('autoprefixer')()
+//   }
+// }
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link to="/"></router-link>
+    <router-link to="/home"></router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
